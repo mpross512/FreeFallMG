a8eb2f90b6e3fd84e1fd118a68e4889e2f57cc9d
mpross512 <mpross512@gmail.com>
2018-11-28 14:53:48 -0600

    Added Entities

---
+++ FreeFall/Entity/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace FreeFall.Entity
+{
+    public class Player
+    {
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Player()
+        {
+        }
+
+        public void 
+    }
+}
